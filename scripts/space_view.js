1310e1da46357f077ec835368ce66806b835a244
Sami Hattab <hattab_sami@yahoo.fr>
2019-05-10 12:13:43 +0200

    add a callback function for the spawn of the textures

--- scripts/space_view.js
+++ scripts/space_view.js
@@ -1,13 +1,9 @@
-const loader = new THREE.TextureLoader();
-const earth_texture = loader.load("../assets/images/texture_earth-5400x2700.jpg");
-const moon_texture = loader.load("../assets/images/texture_moon-2048x1024.jpg");
-const stars = loader.load("../assets/images/stars-1920x1080.jpg");
 
 const axis = new THREE.Vector3(0, 1, 0).normalize();
 
 // Scene
 const scene = new THREE.Scene();
-scene.background = stars;
+
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 5000);
@@ -27,18 +23,8 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new THREE.OrbitControls( camera , renderer.domElement);
 
-// Earth Mesh
-const earth_geometry = new THREE.SphereGeometry(15, 32, 32);
-const earth_material = new THREE.MeshPhongMaterial({ map: earth_texture });
-const earth = new THREE.Mesh(earth_geometry, earth_material);
-earth.position.set(0, 0, 0)
 
 
-// Moon Mesh
-const moon_geometry = new THREE.SphereGeometry(5, 32, 32);
-const moon_material = new THREE.MeshPhongMaterial({ map: moon_texture });
-const moon = new THREE.Mesh(moon_geometry, moon_material);
-moon.position.set(-40, 0, 0);
 
 
 // Light
@@ -48,22 +34,46 @@ const directionnalLight = new THREE.DirectionalLight(0xffffff, 2, 0, 2);
 directionnalLight.position.set(50, 0, 30);
 directionnalLight.castShadow = true;
 
-scene.add(ambientLight, directionnalLight, earth, moon);
+scene.add(ambientLight, directionnalLight);
 
-function animate() {
-    requestAnimationFrame(animate);
-    render();
-  }
+let earth;
+const loader = new THREE.TextureLoader();
 
-  var quaternion = new THREE.Quaternion();
-  function render() {
+const earth_texture = loader.load("../assets/images/texture_earth-5400x2700.jpg", function (texture) {
+  // Earth Mesh
+  const earth_geometry = new THREE.SphereGeometry(15, 32, 32);
+  const earth_material = new THREE.MeshPhongMaterial({ map: texture });
+  earth = new THREE.Mesh(earth_geometry, earth_material);
+  earth.position.set(0, 0, 0);
+  scene.add(earth);
+});
+
+let moon;
+const moon_texture = loader.load("../assets/images/texture_moon-2048x1024.jpg", function (texture) {
+  // Moon Mesh
+  const moon_geometry = new THREE.SphereGeometry(5, 32, 32);
+  const moon_material = new THREE.MeshPhongMaterial({ map: texture });
+  moon = new THREE.Mesh(moon_geometry, moon_material);
+  moon.position.set(-40, 0, 0);
+  scene.add(moon);
+});
+
+
+const stars = loader.load("../assets/images/stars-1920x1080.jpg");
+scene.background = stars;
+
+const quaternion = new THREE.Quaternion();
+function render() {
+  if(earth && moon) {
     earth.rotation.y += 0.001;
     moon.rotation.y += 0.001;
-
     quaternion.setFromAxisAngle(axis, -0.008);
     moon.position.applyQuaternion(quaternion);
-
-    renderer.render(scene, camera);
+  }
+  
+  renderer.render(scene, camera);
+  
+  requestAnimationFrame(render);
   }
 
-animate();
+render();
